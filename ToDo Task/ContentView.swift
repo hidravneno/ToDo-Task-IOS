32c9115fcfc79ecbb54a2bf2ef0beef1c3e8376b
aramburo reyes francisco eduardo <aramburoeduardo972@gmail.com>
2025-12-09 18:23:24 -0800

    Initial Commit

---
+++ ToDo Task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDo Task
+//
+//  Created by francisco eduardo aramburo reyes on 09/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDo Task/ToDo_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_TaskApp.swift
+//  ToDo Task
+//
+//  Created by francisco eduardo aramburo reyes on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
