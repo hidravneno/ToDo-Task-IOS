b1d8e9c82fd8f65d7b0cfe7d60870061445511ca
aramburo reyes francisco eduardo <aramburoeduardo972@gmail.com>
2025-12-10 18:50:45 -0800

    Add ToDo Task project files

--- ToDo Task/ContentView.swift
+++ ToDo Task/ContentView.swift
@@ -8,17 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    @State private var taskGroups = TaskGroup.sampleData // See MockData
+    @State private var selectedGroup: TaskGroup? // selected group
+    @State private var columnVisibility: NavigationSplitViewVisibility = .all // navigation side panel
 
-#Preview {
-    ContentView()
+    var body: some View {
+        NavigationSplitView(columnVisibility: $columnVisibility) {
+            // SIDEBAR
+            List(selection: $selectedGroup) {
+                ForEach(taskGroups) { group in
+                    NavigationLink(value: group) {
+                        Label(group.title, systemImage: group.symbolName)
+                    }
+                }
+            }
+            .navigationTitle("ToDo APP")
+            .listStyle(.sidebar)
+        } detail: {
+            if let group = selectedGroup {
+                if let index = taskGroups.firstIndex(where: {$0.id == group.id}) {
+                    TaskGroupDetailView(groups: $taskGroups[index])
+                }
+            } else {
+                ContentUnavailableView("Select a Group", systemImage: "sidebar.left")
+            }
+        }
+    }
 }
---
+++ ToDo Task/TaskModels.swift
@@ -0,0 +1,37 @@
+//
+//  TaskModels.swift
+//  ToDo Task
+//
+//  Created by francisco eduardo aramburo reyes on 09/12/25.
+//
+
+import Foundation
+
+struct TaskItem: Identifiable, Hashable {
+    let id = UUID()
+    var title: String
+    var isCompleted: Bool = false
+}
+
+struct TaskGroup: Identifiable, Hashable {
+    let id = UUID()
+    var title: String
+    var symbolName: String
+    var tasks: [TaskItem]
+}
+
+
+// MOCK DATA
+extension TaskGroup {
+    static let sampleData: [TaskGroup] = [
+        TaskGroup(title: "Groceries", symbolName: "storefront.circle.fill", tasks: [
+            TaskItem(title: "Buy Apples"),
+            TaskItem(title: "Buy Milk")
+        ]),
+        
+        TaskGroup(title: "Home", symbolName: "house.fill", tasks: [
+            TaskItem(title: "Walk the dog", isCompleted: true ),
+            TaskItem(title: "Clean the kitchen")
+        ])
+    ]
+}
